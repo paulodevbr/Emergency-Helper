554096248c7f08dd3fa1df2222b9a8acc4f2885d
pauloXtr3m <paulo-gyn@outlook.com>
2016-06-29 22:52:05 -0300

    Tela com lista de telefones agora está funcional

--- Android Studio Project/EmergencyHelper/app/src/main/java/app/com/bugdroidbuilder/paulo/emergencyhelper/view/TelefonesUteisActivity.java
+++ Android Studio Project/EmergencyHelper/app/src/main/java/app/com/bugdroidbuilder/paulo/emergencyhelper/view/TelefonesUteisActivity.java
@@ -1,5 +1,7 @@
 package app.com.bugdroidbuilder.paulo.emergencyhelper.view;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -7,6 +9,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,15 +40,45 @@ public class TelefonesUteisActivity extends AppCompatActivity {
 
         prepareTelefoneData();
 
+        recyclerView.addOnItemTouchListener(new RecyclerViewListener.RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerViewListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                TelefoneUtil telefoneUtil = listaTelefones.get(position);
+
+                String uri = "tel:" + telefoneUtil.getNumero().trim() ;
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+
+                try {
+                    startActivity(intent);
+                }catch (SecurityException e){
+
+                }
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
     }
 
     private void prepareTelefoneData() {
 
-        listaTelefones.add(new TelefoneUtil("SAMU", "192"));
-        listaTelefones.add(new TelefoneUtil("Policia Militar", "190"));
+        listaTelefones.add(new TelefoneUtil("SAMU", getResources().
+                getString(R.string.telefone_samu)));
+        listaTelefones.add(new TelefoneUtil("Corpo de Bombeiros", getResources().
+                getString(R.string.telefone_corpo_de_bombeiros)));
+        listaTelefones.add(new TelefoneUtil("Polícia Militar", getResources().
+                getString(R.string.telefone_policia_militar)));
+        listaTelefones.add(new TelefoneUtil("Polícia Rodoviária Federal", getResources().
+                getString(R.string.telefone_policia_rodoviaria_federal)));
+
         mAdapter.notifyDataSetChanged();
 
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // If the user click to go back, the app goes to MainActivity
